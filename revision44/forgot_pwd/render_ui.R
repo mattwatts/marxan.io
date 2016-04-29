031434793091d71c12814cef6250657a62e39ce2
mattwatts <orsaqa@gmail.com>
2016-04-29 10:38:05 +1000

    rev 44

---
+++ revision44/forgot_pwd/render_ui.R
@@ -0,0 +1,30 @@
+    output$mainui <- renderUI({
+        mainPanel(
+            h2("Reset password for marxan.io"),
+            br(),
+            a("marxan.io user guide", href=paste0("http://marxan.net/downloads/",sUserGuide), target="_blank"),
+            br(),
+            br(),
+            conditionalPanel(condition = "input.showform == '1'",
+                HTML("To reset the password for your account, please enter the email address you registered with below."),
+                br(),
+                br(),
+                conditionalPanel(condition = "input.showhint == '1'",
+                    br(),
+                    HTML(paste0("", tags$span(style="color:red", "Enter email")))
+                ),
+                conditionalPanel(condition = "input.showinvalid == '1'",
+                    br(),
+                    HTML(paste0("", tags$span(style="color:red", "Account doesn't exist")))
+                ),
+                textInput("email","Your email address:"),
+                br(),
+                actionButton('registerUser', 'Reset password'),
+                br()
+            ),
+            conditionalPanel(condition = "input.showok == '1'",
+                HTML("A password reset email has been sent to you. The password reset link will expire in 12 hours."),
+                br()
+            )
+      )
+    })
---
+++ revision44/upload/global.R
@@ -0,0 +1,37 @@
+# marxan.io Upload a Dataset
+
+cat(paste0("hello\n"))
+cat(paste0(getwd(),"\n"))
+
+sAppDir <<- getwd()
+
+sShinyPath <<- "/mnt/shiny/"
+sShinyUserPath <<- paste0(sShinyPath,"users/")
+sShinyDataPath <- paste0(sShinyPath,"data/")
+sShinyTempPath <<- paste0(sShinyPath,"temp/")
+
+iRevision <- 44
+sUserGuide <<- "Marxan_io_rev39_user_guide.pdf"
+
+if (.Platform$pkgType == "source")
+{
+  sShinySourcePath <<- paste0(sShinyPath,"apps/revision",iRevision,"/source/")
+} else {
+  sShinySourcePath <<- paste0("/Users/matt/Documents/R/revision",iRevision,"/source/")
+}
+
+sDatabase <<- ""
+sMarxanDir <<- ""
+irefreshinput <<- 0
+irefreshmrun <<- 0
+isavetargetspf <<- 0
+sBLM <<- 0.1
+sProp <<- 0.3
+sSPF <<- 1
+iSpecDatRows <<- 0
+iCores <<- 10
+iRepsPerCore <<- 10
+
+iParamTestReps <<- 10
+
+
